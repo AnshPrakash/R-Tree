fb3cc620082f4fea88049b9549a983e01ce4e3d1
AnshPrakash <anshprakash1997@gmail.com>
2020-06-24 20:56:44 +0530

    bulk load_check update done but not correct

--- bulk_load.cpp
+++ bulk_load.cpp
@@ -1,28 +1,27 @@
+
 #include "Rtree.h"
 
 
 void Btree::bulk_load(FileHandler& fh_1, FileHandler& fh, int N){
-  PageHandler ph = fh_1.FirstPage();
+  PageHandler ph = fh_1.FirstPage ();
   int pts_per_page = PAGE_CONTENT_SIZE/(sizeof(int)*d);
   int pts_last_page = N%pts_per_page;
   int num_pages = 1 + ((N - 1) / pts_per_page); //ceil quotient
   int cur_page = ph.GetPageNum(); 
   int read_num = 0;
-  std::vector< std::vector<int >> ret; //= std::vector< std::vector< int > >(N,std::vector< int >(2*d,INT_MIN));
+  std::vector< std::vector<int >> ret;
   
-  for(int i=0; i<num_pages; i++){ //or while(true){
+  for(int i=0; i<num_pages; i++){ 
     try{
       int num_ctr=0; //which loc in page is being read
       char *data = ph.GetData ();
       //Assuming each point contains one int & last page contains rest of the vectors
       
-      if(i==num_pages-1)
+      if(i==num_pages-1 && N%pts_per_page!=0) //change
         pts_per_page = pts_last_page;
       
-      //is there an alternative way of reading pts?
-      
       for(int j=0;j<pts_per_page;j++){
-        std::vector< int > push_pt = std::vector< int >(2*d,INT_MIN);
+        std::vector< int > push_pt = std::vector< int >(); //change
         for(int k=0;k<d;k++){
           memcpy (&read_num, &data[num_ctr++], sizeof(int));
           push_pt.push_back(read_num);
@@ -51,7 +50,7 @@ int Btree::Allocate_points(std::vector< std::vector<int >> data, FileHandler& fh
   for(int i = 0;i < num_blocks;i++){
     Node n = AllocateNode(fh,-1);
     rootPageId=n.pageId; //update every time
-    if(i == num_blocks-1)
+    if(i == num_blocks-1 && N%maxCap!=0) //change
       size = N%maxCap;
     n.size = size;
     n.leaf= is_leaf;
@@ -73,6 +72,5 @@ int Btree::Allocate_points(std::vector< std::vector<int >> data, FileHandler& fh
     return Allocate_points(new_data,fh,num_blocks,false,new_pageIds);
   else  
     return rootPageId;
-  //should return root_node too
 }
 
